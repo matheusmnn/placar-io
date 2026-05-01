f4e858764871e3a4221abc07ada9d637ef40efd9
matheusmnn <matheusmartinsnunes@gmail.com>
2026-05-01 14:14:27 -0300

    inicializa servidor Express com middlewares basicos

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.status(200).json({message: 'Rota de testes funcionando'})
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Ouvindo na porta ${PORT}`)
+});
+
